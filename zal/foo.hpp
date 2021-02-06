c1ffeee18cdbd5b3ffbf63eeb13f89de0bbfc919
kpmrozowski <kpmrozowski@gmail.com>
2021-02-06 18:01:51 +0000

    zal->v1.6

--- zal/foo.hpp
+++ zal/foo.hpp
@@ -9,7 +9,7 @@ std::vector< char > foo(std::list< Human >& people)
 {
 	std::vector<char> vc;
 	
-	for(Human h : people) {
+	for(Human& h : people) {
 		h.birthday();
 		vc.push_back(h.isMonster() ? 'n' : 'y');
 	}
